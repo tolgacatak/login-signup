e69b98e4a7798d0bfbb2e382b5194802b99c29c2
tqyl <106453124+tqyl@users.noreply.github.com>
2024-03-13 20:03:43 +0300

    şifreyi unuttum alanında mail gönderilme eklendi

--- src/Components/ForgetPassword/ForgetPassword.jsx
+++ src/Components/ForgetPassword/ForgetPassword.jsx
@@ -1,36 +1,52 @@
-import React from 'react'
-import './ForgetPassword.css'
-import { Link } from 'react-router-dom'
-
-import gorsel from '../Assets/logo.jpeg'
+import React, { useState } from 'react';
+import axios from 'axios';
+import './ForgetPassword.css';
+import { Link, useNavigate } from 'react-router-dom'; // useNavigate ekledik
+import gorsel from '../Assets/logo.jpeg';
 
 const ForgetPassword = () => {
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate(); // useNavigate'i tanımladık
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.get(`http://localhost:8087/users/request/${email}`);
+      navigate('/CodePage');
+    } catch (error) {
+      console.error('Hata:', error);
+    }
+  };
+
   return (
     <div className="container-forget">
-
       <div className="header-forget">
         <div className="img-forget">
           <img src={gorsel} alt="" />
         </div>
-        <div className="text-forget">
-          Şifremi Unuttum
-        </div>
+        <div className="text-forget">Şifremi Unuttum</div>
         <div className="underline-forget"></div>
       </div>
-      <div className="inputs-forget">
-        <div className="label-forget"><h2>Mail Adresinizi Girin</h2></div>
-        <div className="input-forget">
-          <input type="text" placeholder="E-mail" />
+      <form className="inputs-forget" onSubmit={handleSubmit}>
+        <div className="label-forget">
+          <h2>Mail Adresinizi Girin</h2>
         </div>
-      </div>
-      <div className="submit-container-forget">
-        <Link to="/codepage" className="submit-forget">
-          Gönder
-        </Link>
-
-      </div>
+        <div className="input-forget">
+          <input
+            type="text"
+            placeholder="E-mail"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="submit-container-forget">
+          <button type="submit" className="submit-forget">
+            Gönder
+          </button>
+        </div>
+      </form>
     </div>
+  );
+};
 
-  )
-}
-export default ForgetPassword
+export default ForgetPassword;
