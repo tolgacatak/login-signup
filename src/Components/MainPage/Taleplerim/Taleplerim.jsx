e654f0bbc0ad08e5200a18d0941b9225b5902762
tqyl <106453124+tqyl@users.noreply.github.com>
2024-03-29 13:17:14 +0300

    taleplerim şablonu oluşturuldu

--- src/Components/MainPage/Taleplerim/Taleplerim.jsx
+++ src/Components/MainPage/Taleplerim/Taleplerim.jsx
@@ -1,9 +1,16 @@
-
+import Navbar from 'C:\\Users\\User\\Desktop\\login-signup\\login-signup\\src\\Components\\Navbar\\NavbarAnasayfa';
+import Footer from 'C:\\Users\\User\\Desktop\\login-signup\\login-signup\\src\\Components\\Footer\\Footer';
 import './Taleplerim.css'
 
 const Taleplerim = () => {
   return (
-    <div>Taleplerim</div>
+    <div>
+      <Navbar />
+      <div className="taleplerim-wrapper">
+        
+      </div>
+      <Footer />
+    </div>
   )
 }
 
