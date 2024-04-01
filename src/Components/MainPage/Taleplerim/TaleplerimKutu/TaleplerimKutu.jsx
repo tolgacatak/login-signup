734954c12f262c07908984efcc10bc9170eaed63
tqyl <106453124+tqyl@users.noreply.github.com>
2024-04-01 09:54:08 +0300

    aktif talep kutu tamamlandı

--- src/Components/MainPage/Taleplerim/TaleplerimKutu/TaleplerimKutu.jsx
+++ src/Components/MainPage/Taleplerim/TaleplerimKutu/TaleplerimKutu.jsx
@@ -8,8 +8,14 @@ const TaleplerimKutu = () => {
         <div>
             <div className="talep-kutu-wrapper">
                 <div className="talep-kutu-soltaraf">
+                    <div className="talep-kutu-userimg">
+                        <img src={logo} alt="" />
+                    </div>
                     <div className="talep-kutu-gorsel">
                         <img src={kutu} alt="" />
+                    </div>
+                    <div className="talep-kutu-adsoyad">
+                        <h4>Sayın İsim Soyisim</h4>
                     </div>
                     <div className="talep-kutu-aktif">
                         <h2>AKTİF</h2>
@@ -19,19 +25,25 @@ const TaleplerimKutu = () => {
                 </div>
                 <div className="talep-kutu-sagtaraf">
                     <div className="talep-kutu-title">
-                        <h4>Şehit Ahmet İlkokulu Yardım Kutusu</h4>
+                        <h4>Hatay - Şehit Ahmet İlkokulu Yardım Kutusu</h4>
                     </div>
                     <div className="talep-kutu-content">
                         <p>VAN'da bulunan Şehit Ahmet İlkokulu'nun değerli öğrencilerine yönelik olarak açılan bu yardım kutusu, onların eğitim hayatlarını desteklemek ve günlük ihtiyaçlarını karşılamak amacıyla hazırlandı. Bu kutuda, öğrencilerimiz için gerekli olan kırtasiye malzemeleri, okul çantaları, kitaplar ve giysi gibi temel eğitim materyallerini toplamayı hedefliyoruz. Her çocuğun eğitimine erişiminin önemli olduğuna inanarak, siz değerli bağışçıların desteğiyle onlara daha iyi bir öğrenim ortamı sunmayı amaçlıyoruz. Şehit Ahmet İlkokulu öğrencilerinin eğitimine katkıda bulunmak ve onlara daha parlak bir gelecek sunmak için bağışlarınızı bekliyoruz. Her katkınız, onların hayatında büyük bir fark yaratacaktır. Destekleriniz için şimdiden teşekkür ederiz!</p>
                     </div>
-                </div>
-                <div className="talep-kutu-yorum">
-                    <img src={logo} alt="" />
-                    <div className="talep-kutu-input">
-                        <input type="text" placeholder="Yorumunuzu buraya yazınız..." />
+                    <div className="talep-kutu-adres">
+                        <p>Şehit Ahmet İlkokulu, Yenişehir Mahallesi, Çiçek Sokak No:15,Çayyolu</p>
                     </div>
-                    <div className="talep-kutu-uc-yorum">3 Yorum</div>
+                    <div className="talep-kutu-ozet">
+                        <p>İlkokul çocuklarına destek</p>
+                    </div>
                 </div>
+                    <div className="talep-kutu-yorum">
+                        <img src={logo} alt="" />
+                        <div className="talep-kutu-input">
+                            <input type="text" placeholder="Yorumunuzu buraya yazınız..." />
+                        </div>
+                        <div className="talep-kutu-uc-yorum">3 Yorum</div>
+                    </div>
             </div>
         </div>
     )
