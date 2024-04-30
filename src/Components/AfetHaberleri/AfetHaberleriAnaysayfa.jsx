09ca458336ba422b3dd74a994337dc45dfc5c97a
tqyl <106453124+tqyl@users.noreply.github.com>
2024-04-30 17:07:18 +0300

    anasayfa büyük resim tamamlandı

--- src/Components/AfetHaberleri/AfetHaberleriAnaysayfa.jsx
+++ src/Components/AfetHaberleri/AfetHaberleriAnaysayfa.jsx
@@ -6,45 +6,72 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 import HaberCard from './HaberCard/HaberCard'
+import { useEffect } from 'react'
 
 const AfetHbaerleriAnaysayfa = () => {
-  const images = [foto];
   
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [imageUrl, setImageUrl] = useState('');
+  const [page, setPage] = useState(0);
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [link, setLink] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`http://localhost:8087/disaster-news/paging?page=${page}`);
+        const data = await response.json();
+        const firstImageUrl = data.content.length > 0 ? data.content[0].imageUrl : ''; // İlk resmin URL'sini al
+        const firstItem = data.content.length > 0 ? data.content[0] : {};
+        setImageUrl(firstImageUrl);
+        setTitle(firstItem.title);
+        setDescription(firstItem.description);
+        setLink(firstItem.link); // Linki ayarla
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, [page]);
 
   const handlePrev = () => {
-    setActiveIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+    setPage(prevPage => Math.max(prevPage - 1, 0));
   };
 
   const handleNext = () => {
-    setActiveIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+    setPage(prevPage => prevPage + 1);
+  };
+  const handleClick = () => {
+    window.open(link, '_blank'); // Yeni sekmede aç
   };
   return (
     <div>
       <Navbar />
         <div className="haberler-anasayfa">
-          <div className="buyuk-resim">
+          <div className="buyuk-resim" >
             <div className="resim">
-              <img src={images[activeIndex]} alt="" />
+              <img src={imageUrl} alt="" onClick={handleClick} />
               <div className="pagination">
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
-                <span className="dot"></span>
+                  {[...Array(10)].map((_, index) => (
+                      <span
+                          key={index}
+                          className={`dot ${index === page ? 'active' : ''}`}
+                      ></span>
+                  ))}
               </div>
               <div className="left-arrow">
-                <FontAwesomeIcon icon={faChevronLeft} size='2x'/>
+                <FontAwesomeIcon icon={faChevronLeft} size='2x' onClick={handlePrev}/>
               </div>
               <div className="right-arrow">
-                <FontAwesomeIcon icon={faChevronRight} size='2x' />
+                <FontAwesomeIcon icon={faChevronRight} size='2x' onClick={handleNext}/>
               </div>
             </div>
             <div className="anasyafa-baslik">
-              <h2>Başlık</h2>
+              <h2>{title}</h2>
             </div>
             <div className="anasayfa-icerik">
-              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam eveniet suscipit sunt ducimus, quis ipsam rem ab quos voluptate tempore veniam voluptas aspernatur sapiente consequatur. Consequuntur fuga iusto unde magni totam similique minus reprehenderit maiores? Odit tenetur atque facilis molestias!</p>
+              <p>{description}</p>
             </div>
             <div className="anasayfa-baslik">
               <h2>Afet Haberleri</h2>
