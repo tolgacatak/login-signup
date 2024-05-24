97d73ba7e9e8acaf0071d5a48baa4a12b96b0ab3
tqyl <106453124+tqyl@users.noreply.github.com>
2024-05-24 15:57:28 +0300

    bug fix

--- src/Components/MainPage/Mainpage.jsx
+++ src/Components/MainPage/Mainpage.jsx
@@ -1,47 +1,40 @@
-import NavbarAnasayfa from "../Navbar/NavbarAnasayfa"
-import Footer from "../Footer/Footer"
-import Profil from "./Profil/Profil"
-import depremHaberi from '../Assets/DepremHaberi.png'
-import './Mainpage.css'
-import search from '../Assets/search.png'
-import Sosyalmedya from "./Sosyalmedya/Sosyalmedya"
-import TalepCard from "./TalepCard/TalepCard"
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import LoginModal from '../Modals/Loginmodal'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
-import BulunamadiModal from "../Modals/BulunamadiModal/BulunamadiModal"
-
-
+import NavbarAnasayfa from "../Navbar/NavbarAnasayfa";
+import Footer from "../Footer/Footer";
+import Profil from "./Profil/Profil";
+import './Mainpage.css';
+import search from '../Assets/search.png';
+import Sosyalmedya from "./Sosyalmedya/Sosyalmedya";
+import TalepCard from "./TalepCard/TalepCard";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import BulunamadiModal from "../Modals/BulunamadiModal/BulunamadiModal";
 
 const Mainpage = () => {
     const [authorized, setAuthorized] = useState(false);
     const id = localStorage.getItem('userId');
-    const[error, setError] = useState(false);
+    const [error, setError] = useState(false);
     const [imageUrl, setImageUrl] = useState('');
     const [page, setPage] = useState(0);
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [link, setLink] = useState('');
     const [newsData, setNewsData] = useState([]);
-    const [newsIndex, setNewsIndex] = useState(0);
     const [searchText, setSearchText] = useState('');
     const [previousNewsData, setPreviousNewsData] = useState([]);
     const [showNoResultModal, setShowNoResultModal] = useState(false);
-    const [showModal, setShowModal] = useState(false);
     const [contentPage, setContentPage] = useState(0);
-    const [tweetId, setTweetId] = useState('');
+    const [newsData2, setNewsData2] = useState([]);
+    const [showSearch, setShowSearch] = useState(false);
 
     useEffect(() => {
         const checkAuthorization = async () => {
             try {
                 const response = await axios.get(`http://localhost:8087/users/checkNameSurname/${id}`);
                 if (response.status === 200) {
-                    console.log("deneme");
                     setAuthorized(true);
-                }
-                else {
+                } else {
                     setAuthorized(false);
                 }
             } catch (error) {
@@ -52,7 +45,7 @@ const Mainpage = () => {
             checkAuthorization();
         }
     }, [id]);
-    
+
     useEffect(() => {
         async function fetchData() {
             try {
@@ -71,21 +64,20 @@ const Mainpage = () => {
 
         fetchData();
     }, [page]);
+
     useEffect(() => {
         async function fetchTweet() {
             try {
                 const response = await axios.get(`http://localhost:8085/api/contents/paging?page=${contentPage}`);
                 setNewsData(response.data.content);
                 setPreviousNewsData(response.data.content);
-                setTweetId(response.data.content[contentPage].tweetId);
-                
-
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         }
         fetchTweet();
-    }, []);
+    }, [contentPage]);
+
     const handlePrev = () => {
         setPage(prevPage => Math.max(prevPage - 1, 0));
     };
@@ -97,97 +89,94 @@ const Mainpage = () => {
     const handleClick = () => {
         window.open(link, '_blank');
     };
+
     const yardimalHandler = () => {
-        
-        setError(
-            {
-                title: "Hata!",
-                message: 'Taleplerinizi görüntüleyebilmek için isim ve soyisim bilgilerinizi eklemelisiniz!'
-            }
-        )
-    
-}
-const errorHandler = () => {
-    setError('');
-}
-const handleSearch = () => {
-    axios.get(`http://localhost:8085/api/contents/search?text=${searchText}`)
-        .then(response => {
-            setNewsData(response.data);
-            if (response.data.length === 0) {
-                setShowNoResultModal(true);
-            } else {
-                setShowNoResultModal(false);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching data: ', error);
-            setShowNoResultModal(true); // Eğer hata alınırsa da modalı göster
+        setError({
+            title: "Hata!",
+            message: 'Taleplerinizi görüntüleyebilmek için isim ve soyisim bilgilerinizi eklemelisiniz!'
         });
+    };
+
+    const errorHandler = () => {
+        setError('');
+    };
+
+    const handleSearch = () => {
+        axios.get(`http://localhost:8085/api/contents/search?text=${searchText}`)
+            .then(response => {
+                setNewsData2(response.data);
+                setShowSearch(true);
+                if (response.data.length === 0) {
+                    setShowNoResultModal(true);
+                } else {
+                    setShowNoResultModal(false);
+                }
+            })
+            .catch(error => {
+                console.error('Error fetching data: ', error);
+                setShowNoResultModal(true);
+            });
+    };
+
+    const handleInputChange = (e) => {
+        setSearchText(e.target.value);
+        if (e.target.value === '') {
+            setNewsData(previousNewsData);
+            setShowSearch(false);
+        }
+    };
+
+    const onClose = () => {
+        setShowNoResultModal(!showNoResultModal);
+    };
+
+    return (
+        <div>
+            <NavbarAnasayfa />
+            {showNoResultModal && <BulunamadiModal onClose={onClose} />}
+            <Profil />
+            <div className="deprem-haberi">
+                <span>Afet Haberleri</span>
+                <img src={imageUrl} alt="" onClick={handleClick} />
+                <div className="pagination2">
+                    {[...Array(10)].map((_, index) => (
+                        <span
+                            key={index}
+                            className={`dot2 ${index === page ? 'active' : ''}`}
+                        ></span>
+                    ))}
+                </div>
+                <div className="left-arro2">
+                    <FontAwesomeIcon icon={faChevronLeft} size='2x' onClick={handlePrev} />
+                </div>
+                <div className="right-arro2">
+                    <FontAwesomeIcon icon={faChevronRight} size='2x' onClick={handleNext} />
+                </div>
+            </div>
+            <div className="talep-card-mainpage">
+                <TalepCard authorized={authorized} />
+            </div>
+            <div className="arama-anasayfa">
+                <span>Afet Adı veya Anahtar Kelime Giriniz(ör: 'Deprem','Sel' vb.)</span>
+                <div className="arama">
+                    <input type="text" placeholder="Aramak İçin Tıklayın..." value={searchText} onChange={handleInputChange} />
+                    <img src={search} alt="" onClick={handleSearch} />
+                </div>
+            </div>
+            <div className="sosyal-medya-haberler">
+                {showSearch ? (
+                    newsData2.map((tweet, index) => (
+                        <Sosyalmedya key={index} tweet={tweet} />
+                    ))
+                ) : (
+                    newsData.map((tweet, index) => (
+                        <Sosyalmedya key={index} tweet={tweet} />
+                    ))
+                )}
+            </div>
+            <Footer />
+        </div>
+    );
 };
 
-
-const handleInputChange = (e) => {
-    setSearchText(e.target.value);
-    if (e.target.value === '') {
-        setNewsData(previousNewsData);
-    }
-};
-
-const onClose = () => {
-    setShowNoResultModal(!showNoResultModal);
-}
-
-  return (
-    <div>
-        <NavbarAnasayfa/>
-        {showNoResultModal && <BulunamadiModal onClose={onClose}/>}
-        <Profil />
-        <div className="deprem-haberi">
-            <span>Afet Haberleri</span>
-        <img src={imageUrl} alt="" onClick={() => handleClick(link)} />
-            {/* <div className="deprem-haberi-text">
-                <span>{title}</span>
-            </div> */}
-            <div className="pagination2">
-              {[...Array(10)].map((_, index) => (
-                <span
-                  key={index}
-                  className={`dot2 ${index === page ? 'active' : ''}`}
-                ></span>
-              ))}
-            </div>
-            <div className="left-arro2">
-              <FontAwesomeIcon icon={faChevronLeft} size='2x' onClick={handlePrev}/>
-            </div>
-            <div className="right-arro2">
-              <FontAwesomeIcon icon={faChevronRight} size='2x' onClick={handleNext}/>
-            </div>
-            
-        </div>
-        <div className="talep-card-mainpage">
-            <TalepCard authorized={authorized} />
-        </div>
-        <div className="arama-anasayfa">
-            <span>Afet Adı veya Anahtar Kelime Giriniz(ör: 'Deprem','Sel' vb.)</span>
-            <div className="arama">
-                <input type="text" placeholder="Aramak İçin Tıklayın..." value={searchText} onChange={handleInputChange} />
-                <img src={search} alt="" onClick={handleSearch} />
-            </div>
-        </div>
-        <div className="sosyal-medya-haberler">
-            
-                {newsData.map((tweet, index) => (
-                    <Sosyalmedya key={index} tweet={tweet} />
-                ))}
-        </div>
-        
-
-
-        <Footer 
-        />
-    </div>
-  )
-}
-
-export default Mainpage
+export default Mainpage;
--- src/Components/MainPage/Sosyalmedya/Sosyalmedya.jsx
+++ src/Components/MainPage/Sosyalmedya/Sosyalmedya.jsx
@@ -124,12 +124,20 @@ const Sosyalmedya = ({ tweet: initialTweet }) => {
 
     const formatDate = (timestamp) => {
         const date = new Date(timestamp);
+    
+        // Adjust the date by the timezone offset
+        const offset = date.getTimezoneOffset();
+        date.setMinutes(date.getMinutes() + offset);
+    
         const day = date.getDate().toString().padStart(2, '0');
         const month = (date.getMonth() + 1).toString().padStart(2, '0');
         const year = date.getFullYear().toString();
-        return `${day}-${month}-${year}`;
-        
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        return `${day}-${month}-${year} ${hours}:${minutes}`;
     };
+    
+    
 
     const hideUrls = (text) => {
         const urlRegex = /(https?:\/\/[^\s]+)/g;
